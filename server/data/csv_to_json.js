dd1cfc8c2b19ca86e08cbd797227b8d096bef109
HirunaHarankahadeniya <hiruna.19@cse.mrt.ac.lk>
2023-10-23 01:01:15 +0530

    Created data folder

---
+++ server/data/csv_to_json.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+let scvData = []
+fs.readFile("server/corpus.csv", "utf-8", (err, data) => {
+     if (err) throw err;
+     // console.log(data);
+     scvData = data.split("\n");
+     for (let i = 0; i < scvData.length; i++) {
+          scvData[i] = scvData[i].replace("\r", "").split(",");
+     }
+     let headers = scvData.shift();
+     let jsonData = []
+     json_index = {"index": {"_index": "metaphors"}}
+     for (let i = 0; i < scvData.length; i++) {
+          jsonData.push(json_index);
+          let temp = {};
+          for (let j = 0; j < headers.length; j++) {
+               temp[headers[j]] = scvData[i][j];
+          }
+          jsonData.push(temp);
+     }
+     function appendData() {
+          if (jsonData.length) {
+               fs.appendFile("server/corpus.json", JSON.stringify(jsonData.shift())+"\n", (err) => {
+                    if (err) throw err;
+                    appendData();
+               });
+          }
+     }
+     appendData(); 
+});
